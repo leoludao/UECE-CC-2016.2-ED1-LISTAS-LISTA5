61122b9a8f420ca5f05268346308bedebd75e735
leoludao <leoludao@gmail.com>
2017-05-28 19:47:44 -0300

    Add files via upload

---
+++ Lista6-BARALHO.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*professor Evilazio, játive uma idéia para fazer esse código usando Enumeração(enum),mas ainda não tive tempo de implementar... se o prazo for prorrogado posso tentar...senão mando somente o código sobre o problema Josephus  que está no outro arquivo já carregado no git hub */
